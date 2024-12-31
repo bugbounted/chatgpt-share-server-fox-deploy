813f5b1b23d3ce5f33bc6caab1dbd1b062d6fe9f
zhoujy <672192090@qq.com>
2024-12-31 17:34:12 +0800

    修改部署脚本

--- quick-install.sh
+++ quick-install.sh
@@ -3,10 +3,10 @@ set -e
 
 ## 克隆仓库到本地
 echo "clone repository..."
-git clone --depth=1 https://github.com/xiaomifengd/chatgpt-share-fox-deploy.git chatgpt-share-fox
+git clone --depth=1 https://github.com/xiaomifengd/chatgpt-share-server-fox-deploy.git chatgpt-share-server-fox
 
 ## 进入目录
-cd chatgpt-share-fox
+cd chatgpt-share-server-fox
 
 # docker compose pull
 docker compose up -d --remove-orphans
