66ae3d430695ecf3137c85804f62bb082181b4a9
kedish <672192090@qq.com>
2025-01-02 23:00:56 +0800

    修改文言

--- quick-install.sh
+++ quick-install.sh
@@ -10,8 +10,8 @@ git clone --depth=1 https://github.com/xiaomifengd/chatgpt-share-server-fox-depl
 cd chatgpt-share-server-fox
 # 默认值
 DEFAULT_CHATPROXY=""
-read -p "请输入CHATPROXY地址 [$DEFAULT_CHATPROXY]: " CHATPROXY
-
+echo -n "请输入xyhelper网关："
+read CHATPROXY < /dev/tty
 # 如果用户直接回车，使用默认值
 CHATPROXY=${CHATPROXY:-$DEFAULT_CHATPROXY}
 
@@ -21,7 +21,7 @@ sed -i "s|CHATPROXY: \".*\"|CHATPROXY: \"$CHATPROXY\"|" docker-compose.yml
 
 # cat docker-compose.yml
 # docker compose pull
-# docker compose up -d --remove-orphans
+docker compose up -d --remove-orphans
 
 ## 提示信息
 echo "服务启动成功，请访问 http://localhost:8300"
